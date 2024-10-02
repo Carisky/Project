053ceadfd4c839f4291f22c15900791bb1520de0
Yehor <egorkorsungit@gmail.com>
2024-10-02 18:42:47 +0200

    test connection with server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
 import logo from './logo.svg';
 import './App.scss';
+import axios from 'axios';
+
 
 function App() {
+
+  axios.get('http://localhost:5000/api/')
+  .then(response => console.log(response.data))
+  .catch(error => console.log(error));
+
   return (
     <div className="App">
       <header className="App-header">

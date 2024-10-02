b3f9af41c40b1d17ee368a054777ff2fe91470d9
Yehor <egorkorsungit@gmail.com>
2024-10-02 18:42:29 +0200

    added cors middleware

---
+++ backend/src/middlewares/corsMiddleware.ts
@@ -0,0 +1,12 @@
+import cors from 'cors';
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
+
+const corsMiddleware = cors(corsOptions);
+
+export default corsMiddleware;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import corsMiddleware from './middlewares/corsMiddleware';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT; 
 
-app.get('/', (req, res) => {
+app.use(corsMiddleware);
+
+app.get('/api/', (req, res) => {
   res.send('Hello, TypeScript with Express!');
 });
 

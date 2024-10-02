b86e5659b3081a7230cc256eaf5b7808aa315ffa
Yehor <egorkorsun7@gmail.com>
2024-10-02 17:31:24 +0200

    migration for users table

---
+++ backend/migrations/20241002142315_add_users_table.ts
@@ -0,0 +1,15 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable("users", (table) => {
+    table.increments("id").primary();
+    table.string("name").notNullable();
+    table.string("email").unique().notNullable();
+    table.string("password").notNullable();
+    table.timestamps(true, true);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable("users");
+}

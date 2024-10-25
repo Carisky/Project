7323e54e2fd62dbd96293ae73849626fd647ec28
Yehor <egorkorsungit@gmail.com>
2024-10-25 20:59:20 +0200

    now using new middlware for jwt and swagger for docs

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,49 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import corsMiddleware from './middlewares/corsMiddleware';
+import userRoutes from './routes/userRoutes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsDoc from 'swagger-jsdoc';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT; 
 
+// Middleware
 app.use(corsMiddleware);
+app.use(express.json());
+
+// Swagger setup
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'My API',
+      version: '1.0.0',
+      description: 'API documentation for my project',
+    },
+    servers: [
+      {
+        url: `http://localhost:${port}`,
+      },
+    ],
+  },
+  apis: ['./src/controllers/*.ts'], // Adjust this path as needed
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+
+// Define routes
+app.use('/api/users', userRoutes);
 
 app.get('/api/', (req, res) => {
   res.send('Hello, TypeScript with Express!');
 });
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });

7a22bbea3aa7e9a5255d30d565fd4a1de88f7e4e
Yehor <egorkorsungit@gmail.com>
2024-10-25 20:58:53 +0200

    added router to route requests from client side

---
+++ backend/src/routes/userRoutes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { registerUser, loginUser,deleteUserAccount,getUserProfile,updateUserProfile } from '../controllers/userController';
+import jwtMiddleware from '../middlewares/jwtMiddleware';
+
+const router = Router();
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/profile', jwtMiddleware, getUserProfile);
+router.put('/profile', jwtMiddleware, updateUserProfile);
+router.delete('/profile', jwtMiddleware, deleteUserAccount);
+export default router;

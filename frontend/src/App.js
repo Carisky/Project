6925f8048d3fe2b95d4f54ea10124a8574c8a925
Yehor <egorkorsungit@gmail.com>
2024-11-06 20:58:46 +0100

    added frontend routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
 import './App.scss';
 import axios from 'axios';
-
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AuthPage from './pages/AuthPage';
+import MainPage from './pages/MainPage';
 function App() {
 
   axios.get('http://localhost:5000/api/')
@@ -10,22 +11,12 @@ function App() {
   .catch(error => console.log(error));
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<AuthPage />} />
+      </Routes>
+    </Router>
   );
 }
 

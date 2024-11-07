7d4395d5b2c69f4cde4941293664d8ad7750d37f
Yehor <egorkorsungit@gmail.com>
2024-11-07 20:47:15 +0100

    added palete (light)

---
+++ frontend/src/palete.js
@@ -0,0 +1,15 @@
+const palete = {
+    light : {
+        mainColor:"#8268F8",
+        secondaryColor:"#FE7411",
+        background:"#F5F0ED",
+        mainText:"#2B2B2B",
+        secondaryText:"#808080",
+        accentText:"#FF2A2A",
+    },
+    dark : {
+
+    }
+}
+
+export default palete

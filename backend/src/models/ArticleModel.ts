360b9aa4edfa7b9570828b71ce4986c2d69a812e
Yehor <egorkorsungit@gmail.com>
2024-11-10 14:47:42 +0100

    linked category to articles

--- backend/src/models/ArticleModel.ts
+++ backend/src/models/ArticleModel.ts
@@ -1,9 +1,10 @@
 import { Model } from 'objection';
 import SellerModel from './SellerModel';
-
+import CategoryModel from './CategoryModel'
 class ArticleModel extends Model {
   id!: number;
   seller_id!: number;
+  category_id!: number;
   name!: string;
   amount!: number;
   price!: number;
@@ -24,35 +25,43 @@ class ArticleModel extends Model {
           to: 'sellers.id',
         },
       },
+      category: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: CategoryModel,
+        join: {
+          from: 'articles.category_id',
+          to: 'categories.id',
+        },
+      },
     };
   }
 
-  // Поиск статьи по ID
+  // Поиск Товара по ID
   static async findById(id: number): Promise<ArticleModel | undefined> {
     return await ArticleModel.query().findById(id);
   }
 
-  // Поиск статьи по имени
+  // Поиск Товара по имени
   static async findByName(name: string): Promise<ArticleModel | undefined> {
     return await ArticleModel.query().where('name', name).first();
   }
 
-  // Получение всех статей
+  // Получение всех Товаров
   static async findAll(): Promise<ArticleModel[]> {
     return await ArticleModel.query();
   }
 
-  // Создание новой статьи
+  // Создание нового Товара
   static async createArticle(data: Partial<ArticleModel>): Promise<ArticleModel> {
     return await ArticleModel.query().insert(data).returning('*');
   }
 
-  // Обновление данных статьи по ID
+  // Обновление данных Товара по ID
   static async updateArticle(id: number, updates: Partial<ArticleModel>): Promise<ArticleModel | null> {
     return await ArticleModel.query().patchAndFetchById(id, updates);
   }
 
-  // Удаление статьи по ID
+  // Удаление Товара по ID
   static async deleteArticle(id: number): Promise<number> {
     return await ArticleModel.query().deleteById(id);
   }

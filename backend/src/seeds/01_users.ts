4a65f927d0c53ed3922fa6d741f8b799804bfdd5
Yehor <egorkorsungit@gmail.com>
2024-11-10 14:43:34 +0100

    hashing for passwords in test users added

--- backend/src/seeds/01_users.ts
+++ backend/src/seeds/01_users.ts
@@ -1,13 +1,24 @@
 import type { Knex } from "knex";
+import bcrypt from 'bcryptjs';
 
 export async function seed(knex: Knex): Promise<void> {
   // Deletes ALL existing entries
   await knex("users").del();
 
+
+  const hashedApass = await hashPassword("password1");
+  const hashedBpass = await hashPassword("password12");
+  const hashedCpass = await hashPassword("password123");
+
+
   // Inserts seed entries
   await knex("users").insert([
-    { id: 1, name: "Alice", email: "alice@example.com", password: "password123", role: "user" },
-    { id: 2, name: "Bob", email: "bob@example.com", password: "password123", role: "user" },
-    { id: 3, name: "Charlie", email: "charlie@example.com", password: "password123", role: "user" },
+    { id: 1, name: "Alice", email: "alice@example.com", password: hashedApass, role: "user" },
+    { id: 2, name: "Bob", email: "bob@example.com", password: hashedBpass, role: "user" },
+    { id: 3, name: "Charlie", email: "charlie@example.com", password: hashedCpass, role: "user" },
   ]);
+}
+async function hashPassword(password: string) {
+  const saltRounds = 10;
+  return bcrypt.hash(password, saltRounds);
 }
--- backend/src/seeds/02_sellers.ts
+++ backend/src/seeds/02_sellers.ts
@@ -1,12 +1,22 @@
 import type { Knex } from "knex";
+import bcrypt from 'bcryptjs';
 
 export async function seed(knex: Knex): Promise<void> {
+  // Wait for the password hashing to complete
+  const hashedApass = await hashPassword("shopApass");
+  const hashedBpass = await hashPassword("shopBpass");
+
   // Deletes ALL existing entries
   await knex("sellers").del();
 
   // Inserts seed entries
   await knex("sellers").insert([
-    { id: 1, name: "Shop A", email: "shopA@example.com", password: "shopApass", billing_info: "1234-5678-9012-3456" },
-    { id: 2, name: "Shop B", email: "shopB@example.com", password: "shopBpass", billing_info: "2345-6789-0123-4567" },
+    { id: 1, name: "Shop A", email: "shopA@example.com", password: hashedApass, billing_info: "1234-5678-9012-3456", role:"seller" },
+    { id: 2, name: "Shop B", email: "shopB@example.com", password: hashedBpass, billing_info: "2345-6789-0123-4567", role:"seller" },
   ]);
+}
+
+async function hashPassword(password: string) {
+  const saltRounds = 10;
+  return bcrypt.hash(password, saltRounds);
 }

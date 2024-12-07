49b2835c4fcdfc42ff778c9b8f9c0b1aaf131f46
Yehor <egorkorsungit@gmail.com>
2024-12-07 21:31:42 +0100

    switched to array rendering and dummy data for tests

--- frontend/src/components/ArticleList/ArticleList.jsx
+++ frontend/src/components/ArticleList/ArticleList.jsx
@@ -2,135 +2,39 @@ import { Box, Typography } from "@mui/material";
 import React from "react";
 import useTheme from "../../hooks/useTheme";
 import ArticleCarousel from "../ArticleCarousel/ArticleCarousel";
-import ArticleCard from "../ArticleCard/ArticleCard";
-import { useMediaQuery } from "../../hooks/useMediaQuery.js";
 
-export default function ArticleList({ Title }) {
+export default function ArticleList({ Title, articles }) {
   const theme = useTheme();
-  const isMobile = useMediaQuery("(max-width: 500px)");
-  const isTablet = useMediaQuery("(min-width: 500.01px)");
-  const isDesktop = useMediaQuery("(min-width: 1800px)");
+
   return (
-    <>
-      {isMobile &&
-        <Box
+    <Box
+      sx={{
+        height: "550px",
+        width: "82%",
+        margin: "auto",
+        backgroundColor: theme.mainColor,
+        display: "flex",
+        marginTop: "10px",
+        marginBottom: "10px",
+      }}
+    >
+      <Box
         sx={{
-          height: "630px",
-          width:"95%",
-          margin:"auto",
-          backgroundColor: theme.mainColor,
-          marginTop: "10px",
-          marginBottom: "10px",
+          width: "232px",
         }}
       >
-        <Box
+        <Typography
           sx={{
-            width: "95%",
+            color: "white",
+            fontFamily: "Ubuntu",
+            fontSize: "52px",
+            fontWeight: "500",
           }}
         >
-          <Typography
-            sx={{
-              color: "white",
-              fontFamily: "Ubuntu",
-              fontSize: "52px",
-              fontWeight: "500",
-            }}
-          >
-            {Title}
-          </Typography>
-        </Box>
-        <ArticleCarousel>
-            <ArticleCard></ArticleCard>
-            <ArticleCard></ArticleCard>
-            <ArticleCard></ArticleCard>
-            <ArticleCard></ArticleCard>
-            <ArticleCard></ArticleCard>
-            <ArticleCard></ArticleCard>
-            <ArticleCard></ArticleCard>
-            <ArticleCard></ArticleCard>
-            <ArticleCard></ArticleCard>
-        </ArticleCarousel>
-      </Box>}
-      {isTablet &&
-        <Box
-        sx={{
-          height: "550px",
-          width:"82%",
-          margin:"auto",
-          backgroundColor: theme.mainColor,
-          display: "flex",
-          marginTop: "10px",
-          marginBottom: "10px",
-        }}
-      >
-        <Box
-          sx={{
-            width: "232px",
-          }}
-        >
-          <Typography
-            sx={{
-              color: "white",
-              fontFamily: "Ubuntu",
-              fontSize: "52px",
-              fontWeight: "500",
-            }}
-          >
-            {Title}
-          </Typography>
-        </Box>
-        <ArticleCarousel>
-            <ArticleCard></ArticleCard>
-            <ArticleCard></ArticleCard>
-            <ArticleCard></ArticleCard>
-            <ArticleCard></ArticleCard>
-            <ArticleCard></ArticleCard>
-            <ArticleCard></ArticleCard>
-            <ArticleCard></ArticleCard>
-            <ArticleCard></ArticleCard>
-            <ArticleCard></ArticleCard>
-        </ArticleCarousel>
-      </Box>}
-      {isDesktop && 
-        <Box
-        sx={{
-          height: "550px",
-          width:"82%",
-          margin:"auto",
-          backgroundColor: theme.mainColor,
-          display: "flex",
-          marginTop: "10px",
-          marginBottom: "10px",
-        }}
-      >
-        <Box
-          sx={{
-            width: "232px",
-          }}
-        >
-          <Typography
-            sx={{
-              color: "white",
-              fontFamily: "Ubuntu",
-              fontSize: "52px",
-              fontWeight: "500",
-            }}
-          >
-            {Title}
-          </Typography>
-        </Box>
-        <ArticleCarousel>
-            <ArticleCard></ArticleCard>
-            <ArticleCard></ArticleCard>
-            <ArticleCard></ArticleCard>
-            <ArticleCard></ArticleCard>
-            <ArticleCard></ArticleCard>
-            <ArticleCard></ArticleCard>
-            <ArticleCard></ArticleCard>
-            <ArticleCard></ArticleCard>
-            <ArticleCard></ArticleCard>
-        </ArticleCarousel>
-      </Box>}
-    </>
+          {Title}
+        </Typography>
+      </Box>
+      <ArticleCarousel articles={articles}></ArticleCarousel>
+    </Box>
   );
 }
--- frontend/src/components/ArticlePackAdvice/ArticlePackAdvice.jsx
+++ frontend/src/components/ArticlePackAdvice/ArticlePackAdvice.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import { Box, Typography } from '@mui/material';
+import { Box, Typography } from "@mui/material";
 import ArticleCard from "../ArticleCard/ArticleCard";
 import { LogoAdvice } from "../../images/LogoAdvice";
 import { LogoAdvice2 } from "../../images/LogoAdvice2";
 import useTheme from "../../hooks/useTheme";
 import { useMediaQuery } from "../../hooks/useMediaQuery.js";
 
-
-export default function ArticlePackAdvice() {
-
+export default function ArticlePackAdvice({ Title, articles }) {
   const theme = useTheme();
 
   const isMobile = useMediaQuery("(max-width: 500px)");
@@ -16,56 +14,59 @@ export default function ArticlePackAdvice() {
   const isDesktop = useMediaQuery("(min-width: 500.01px)");
 
   return (
-    <>
-    {isMobile &&
-      <Box
+    <Box
       sx={{
         marginTop: "10px",
         marginBottom: "10px",
-        width:"95%",
-        margin:"auto",
+        width: "82%",
+        margin: "auto",
       }}
     >
       <Box
         sx={{
           height: "177px",
-          width: "90%",
-          margin:"auto",
+          width: "882px",
+          margin: "auto",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-          position:"relative",
+          position: "relative",
         }}
       >
-        <Box sx={{
-          position:"absolute",
-          left:"5px",
-          top:"-5px",
-          opacity:1,
-        }}>
+        <Box
+          sx={{
+            position: "absolute",
+            left: "5px",
+            top: "-5px",
+            opacity: 1,
+          }}
+        >
           <LogoAdvice></LogoAdvice>
         </Box>
-        <Box sx={{
-          position:"absolute",
-          right:"-10px",
-          bottom:"-15px",
-          opacity:1,
-        }}>
+        <Box
+          sx={{
+            position: "absolute",
+            right: "-10px",
+            bottom: "-15px",
+            opacity: 1,
+          }}
+        >
           <LogoAdvice2></LogoAdvice2>
         </Box>
-        
+
         <Box
           sx={{
             backgroundColor: "white",
             borderRadius: "20px",
             height: "94px",
-            width: "70%",
-            margin:"auto",
+            width: "746px",
+            margin: "auto",
+            display: "flex",
             justifyContent: "center",
             alignItems: "center",
             boxShadow: "10px 10px 15px rgba(0, 0, 0, 0.7)",
-            zIndex:1,
-            opacity:1,
+            zIndex: 1,
+            opacity: 1,
           }}
         >
           <Typography
@@ -76,105 +77,17 @@ export default function ArticlePackAdvice() {
               marginTop: "5px",
               marginLeft: "15px",
             }}
-          >РЕКОМЕНДАЦІЇ</Typography>
-          <Typography
-            sx={{
-              color: theme.secondaryColor,
-              fontFamily: "Ubuntu",
-              fontSize: "32px",
-              fontWeight: "700",
-              marginTop: "-15px",
-              marginLeft: "15px",
-            }}
-          >ДЛЯ ТЕБЕ</Typography>
+          >
+            {Title}
+          </Typography>
         </Box>
       </Box>
+
       <Box display="flex" justifyContent="space-between">
-        <ArticleCard></ArticleCard>
-        <ArticleCard></ArticleCard>
+        {articles.map((article) => {
+          return <ArticleCard article={article} />;
+        })}
       </Box>
-      <Box display="flex" justifyContent="space-between">
-        <ArticleCard></ArticleCard>
-        <ArticleCard></ArticleCard>
-      </Box>
-    </Box>}
-    {isDesktop &&
-      <Box
-      sx={{
-        marginTop: "10px",
-        marginBottom: "10px",
-        width:"82%",
-        margin:"auto",
-      }}
-    >
-      <Box
-        sx={{
-          height: "177px",
-          width: "882px",
-          margin:"auto",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          position:"relative",
-        }}
-      >
-        <Box sx={{
-          position:"absolute",
-          left:"5px",
-          top:"-5px",
-          opacity:1,
-        }}>
-          <LogoAdvice></LogoAdvice>
-        </Box>
-        <Box sx={{
-          position:"absolute",
-          right:"-10px",
-          bottom:"-15px",
-          opacity:1,
-        }}>
-          <LogoAdvice2></LogoAdvice2>
-        </Box>
-        
-        <Box
-          sx={{
-            backgroundColor: "white",
-            borderRadius: "20px",
-            height: "94px",
-            width: "746px",
-            margin:"auto",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            boxShadow: "10px 10px 15px rgba(0, 0, 0, 0.7)",
-            zIndex:1,
-            opacity:1,
-          }}
-        >
-          <Typography
-            sx={{
-              fontFamily: "Ubuntu",
-              fontSize: "52px",
-              fontWeight: "685",
-            }}
-          >РЕКОМЕНДАЦІЇ</Typography>
-          <Typography
-            sx={{
-              color: theme.secondaryColor,
-              fontFamily: "Ubuntu",
-              fontSize: "52px",
-              fontWeight: "685",
-            }}
-          >ДЛЯ ТЕБЕ</Typography>
-        </Box>
-      </Box>
-      <Box display="flex" justifyContent="space-between">
-        <ArticleCard></ArticleCard>
-        <ArticleCard></ArticleCard>
-        <ArticleCard></ArticleCard>
-        <ArticleCard></ArticleCard>
-        <ArticleCard></ArticleCard>
-      </Box>
-    </Box>}
-    </>
+    </Box>
   );
 }

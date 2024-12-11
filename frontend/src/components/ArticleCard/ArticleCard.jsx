010bb8510c3986cf9cf99642e17411297f71e83a
ArtemBgn <bgn.rtm@gmail.com>
2024-12-11 21:45:49 +0200

    mobile_process

--- frontend/src/components/ArticleCard/ArticleCard.jsx
+++ frontend/src/components/ArticleCard/ArticleCard.jsx
@@ -36,13 +36,18 @@ export default function ArticleCard({article}) {
     <>
     {isMobile &&
       <Box sx={{ margin:"10px", }}>
-        <Card sx={{
-          border: "none",
-          height: "470px",
-          width: "200px",
-          boxShadow: "none",
-          borderRadius: "14px",
-          backgroundColor: theme.mainColor, }}>
+        <Card
+	onMouseDown={handleMouseDown}
+	onMouseUp={handleMouseUp}
+	sx={{
+	userSelect: "none",
+	border: "none",
+	height: "470px",
+	width: "200px",
+	boxShadow: "none",
+	borderRadius: "14px",
+	backgroundColor: theme.mainColor,
+	cursor: "pointer",}}>
           <Box sx={{
             position: "relative",
             display: "flex",
@@ -62,12 +67,15 @@ export default function ArticleCard({article}) {
               marginLeft: "15px",
               marginRight: "15px",
               marginTop: "8px",
-              fontFamily: "Libre Franklin",
-              color: theme.mainText,
-              fontSize: "24px", }}>{article?.name}</Typography>
+              fontFamily: "Libre Franklin",}}>
+		<Typography
+		sx={{
+		color: theme.mainText,
+		fontSize: "24px",}}>{article?.name}</Typography>
             <Typography sx={{
               color: theme.secondaryText,
               fontSize: "24px", }}>{article?.seller_name}</Typography>
+	    </Typography>
           </Box>
           <Box
           sx={{
@@ -84,7 +92,7 @@ export default function ArticleCard({article}) {
               fontWeight: "bolder",
               marginRight: "7px",
               color: theme.mainText,
-            }}>000$
+            }}>{article?.price}
           </Typography>
           <Typography
             sx={{
@@ -125,8 +133,7 @@ export default function ArticleCard({article}) {
               sx={{
                 color: theme.secondaryText,
                 fontSize: "14px",
-              }}>
-              4.8
+              }}>{article?.rating}
             </Typography>
           </Box>
           <Box
@@ -153,153 +160,17 @@ export default function ArticleCard({article}) {
       </Card>
       <Button></Button>
       </Box>}
-    {isTablet &&
-      <Box sx={{
-        margin:"10px"
-      }}>
-      <Card
-        sx={{
-          border:"none",
-          height: "470px",
-          width: "200px",
-          boxShadow:"none",
-          borderRadius:"14px",
-          backgroundColor: theme.mainColor,
-        }}>
-        <Box
-          sx={{
-            position:"relative",
-            display:"flex",
-            borderRadius: "13px",
-            height: "250px",
-            width: "100%",
-            backgroundColor: theme.background,
-          }}>
-          <FavoriteBorderIcon sx={{
-              position:"absolute",
-              top:"16px",
-              right:"16px",
-              cursor:"pointer",
-              zIndex:2
-          }}>
-          </FavoriteBorderIcon>
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-          }}>
-          <Typography
-            sx={{
-              marginLeft: "15px",
-              marginRight: "15px",
-              marginTop: "8px",
-              fontFamily: "Libre Franklin",
-            }}>
-            <Typography
-              sx={{
-                color: theme.mainText,
-                fontSize: "24px",
-              }}>
-              Назва продукту
-            </Typography>
-            <Typography
-              sx={{
-                color: theme.secondaryText,
-                fontSize: "24px",
-              }}>
-              Назва фірми
-            </Typography>
-            <Typography
-              sx={{
-                color: theme.secondaryText,
-                fontSize: "24px",
-              }}></Typography>
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            marginLeft: "15px",
-            marginRight: "15px",
-            marginTop: "7px",
-            fontFamily: "Libre Franklin",
-          }}>
-          <Typography
-            sx={{
-              fontSize: "21px",
-              fontWeight: "bolder",
-              marginRight: "7px",
-              color: theme.mainText,
-            }}>{article?.price}
-          </Typography>
-          <Typography
-            sx={{
-              fontSize: "17px",
-              marginTop: "6px",
-              marginRight: "7px",
-              fontWeight: 400,
-              color: theme.secondaryText,
-            }}>
-            000$
-          </Typography>
-          <Typography
-            sx={{
-              marginTop: "6px",
-              fontSize: "17px",
-              fontWeight: 400,
-              color: theme.accentText,
-            }}>-34%
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            display: "flex",}}>
-              <Box
-            sx={{
-              display: "flex",
-              marginLeft: "15px",
-              marginRight: "15px",
-              marginTop: "7px",}}>
-                <StarIcon
-                sx={{
-                marginRight: "3.5px",
-                color: theme.secondaryColor,}}></StarIcon>
-                <Typography
-              sx={{
-                color: theme.secondaryText,
-                fontSize: "14px",
-              }}>4.8
-            </Typography>
-          </Box>
-          <Box sx={{
-              display: "flex",
-              marginTop: "7px",
-            }}>
-            <ReviewsIcon
-              sx={{
-                height: "20px",
-                width: "20px",
-                marginRight: "3.12px",
-                color: theme.secondaryColor,
-              }}></ReviewsIcon>
-            <Typography
-              sx={{
-                fontSize: "14px",
-                color: theme.secondaryText,
-              }}>
-              23 відгуки
-            </Typography>
-          </Box>
-        </Box>
-      </Card>
-      <Button></Button>
-      </Box>}
+    
     {isDesktop &&
       <Box sx={{
         margin:"10px"
       }}>
       <Card
+	onMouseDown={handleMouseDown}
+	onMouseUp={handleMouseUp}
         sx={{
+	userSelect: "none",
+	cursor: "pointer",
           border:"none",
           height: "470px",
           width: "232px",
@@ -340,16 +211,12 @@ export default function ArticleCard({article}) {
               sx={{
                 color: theme.mainText,
                 fontSize: "24px",
-              }}>
-              Назва продукту
-            </Typography>
+              }}>{article?.name}</Typography>
             <Typography
               sx={{
                 color: theme.secondaryText,
                 fontSize: "24px",
-              }}>
-              Назва фірми
-            </Typography>
+              }}>{article?.seller_name}</Typography>
             <Typography
               sx={{
                 color: theme.secondaryText,
@@ -371,9 +238,7 @@ export default function ArticleCard({article}) {
               fontWeight: "bolder",
               marginRight: "7px",
               color: theme.mainText,
-            }}>
-            000$
-          </Typography>
+            }}>{article?.price}</Typography>
           <Typography
             sx={{
               fontSize: "17px",
@@ -414,7 +279,7 @@ export default function ArticleCard({article}) {
               sx={{
                 color: theme.secondaryText,
                 fontSize: "14px",
-              }}>{article.rating}
+              }}>{article?.rating}
             </Typography>
           </Box>
           <Box
--- frontend/src/components/ArticleList/ArticleList.jsx
+++ frontend/src/components/ArticleList/ArticleList.jsx
@@ -2,39 +2,70 @@ import { Box, Typography } from "@mui/material";
 import React from "react";
 import useTheme from "../../hooks/useTheme";
 import ArticleCarousel from "../ArticleCarousel/ArticleCarousel";
+import { useMediaQuery } from "../../hooks/useMediaQuery.js";
 
 export default function ArticleList({ Title, articles }) {
   const theme = useTheme();
 
+  const isMobile = useMediaQuery("(max-width: 500px)");
+  const isDesktop = useMediaQuery("(min-width: 500.01px)");
+
   return (
-    <Box
-      sx={{
-        height: "550px",
-        width: "82%",
-        margin: "auto",
-        backgroundColor: theme.mainColor,
-        display: "flex",
-        marginTop: "10px",
-        marginBottom: "10px",
-      }}
-    >
-      <Box
+    <>
+      {isMobile &&
+        <Box
         sx={{
           width: "232px",
         }}
       >
-        <Typography
+        <Box
           sx={{
-            color: "white",
-            fontFamily: "Ubuntu",
-            fontSize: "52px",
-            fontWeight: "500",
+            width: "95%",
           }}
         >
-          {Title}
-        </Typography>
-      </Box>
-      <ArticleCarousel articles={articles}></ArticleCarousel>
-    </Box>
+          <Typography
+            sx={{
+              color: "white",
+              fontFamily: "Ubuntu",
+              fontSize: "52px",
+              fontWeight: "500",
+            }}
+          >
+            {Title}
+          </Typography>
+        </Box>
+        <ArticleCarousel articles={articles}></ArticleCarousel>
+      </Box>}
+      {isDesktop && 
+        <Box
+        sx={{
+          height: "550px",
+          width:"82%",
+          margin:"auto",
+          backgroundColor: theme.mainColor,
+          display: "flex",
+          marginTop: "10px",
+          marginBottom: "10px",
+        }}
+      >
+        <Box
+          sx={{
+            width: "232px",
+          }}
+        >
+          <Typography
+            sx={{
+              color: "white",
+              fontFamily: "Ubuntu",
+              fontSize: "52px",
+              fontWeight: "500",
+            }}
+          >
+            {Title}
+          </Typography>
+        </Box>
+        <ArticleCarousel articles={articles}></ArticleCarousel>
+      </Box>}
+    </>
   );
 }
